222622abc04475c82a7182e22a301e1ea6a803eb
stepansmidl <s.smidl@seznam.cz>
2025-12-08 18:03:34 +0100

    feat: added settings page

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -4,8 +4,8 @@ export function Loading(){
     return (
         <Box sx={{flex: 2, justifyContent: "center", alignItems: "center", m: 2}}>
             <Stack direction="column" spacing={2}>
-            <Skeleton animation={"wave"} variant={"rectangular"} sx={{p: 2,height: "14rem", width: "100rem", borderRadius: 3}} />
-            <Skeleton animation={"wave"} variant={"rectangular"} sx={{p: 2,height: "14rem", width: "100rem", borderRadius: 3}} />
+            <Skeleton animation={"wave"} variant={"rectangular"} sx={{p: 2,height: "14rem", width: "100rem", borderRadius: 3, color: "rgba(255,255,255,0.44)"}} />
+            <Skeleton animation={"wave"} variant={"rectangular"} sx={{p: 2,height: "14rem", width: "100rem", borderRadius: 3, color: "rgba(255,255,255,0.44)"}} />
             </Stack>
         </Box>
     )
--- src/Page/SettingsPage.tsx
+++ src/Page/SettingsPage.tsx
@@ -1,6 +1,8 @@
 import {
     Box,
     Typography,
+    Paper,
+    Button
 
 } from "@mui/material";
 
@@ -11,19 +13,36 @@ interface SettignsPageProps{
 
 export function SettingsPage(props : SettignsPageProps){
     return(
-    <Box>
-        <Typography>Settings</Typography>
-        <Box sx={{
-            flexGrow: 1,
-            alignContent: 'center',
-            flexDirection: "column"
-        }}>
-            {props.settings.map((setting) => (
-                <Typography sx={{color: "black"}}>
-                    {setting}
-                </Typography>
+    <Box sx={{}}>
+        <Paper elevation={1} sx={{borderRadius: 2}}>
+            <Typography sx={{fontSize: "2rem", p: 2}}>Settings</Typography>
+            <Box sx={{
+                flexGrow: 1,
+                alignContent: 'top',
+                gap: 1,
+                justifyContent: 'space-between',
+                p: 2,
+                flexDirection: "column",
+                minHeight: "25vh",
+                minWidth:"100%"
+            }}>
+                {props.settings.map((setting) => (
+                    <Typography sx={{color: "black"}}>
+                        {setting}
+                    </Typography>
                 ))}
-        </Box>
+            </Box>
+            <Box sx={{flexGrow: 1,
+                display: "flex",
+                alignItems: "center",
+                p: 2,
+                gap: 2}}>
+                <Button variant={"contained"}>Connect to bride</Button>
+                <Button variant={"contained"}>Reset light settings</Button>
+                <Button variant={"contained"}>Restart hue bridge</Button>
+            </Box>
+        </Paper>
+
     </Box>
     )
 }
